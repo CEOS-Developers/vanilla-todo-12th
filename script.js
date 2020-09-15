46bedbe0ebc5f359657f1a04ee825b04d6eae448
jaeyong <maxwodyd98@gmail.com>
2020-09-15 21:49:17 +0900

    To do List Count 함수 구현

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("To do List init");
+let pendingList = ["To Do List 만들기", "도서관 책 반납하기"];
+let doneList = ["방 청소", "도서관 책 빌리기"];
+
+// 리스트 개수 구하는 함수
+function getListCount() {
+    return {
+        pending: pendingList.length,
+        done: doneList.length
+    }
+}
+
+// 리스트 개수 업데이트 함수
+function updateListCount() {
+    document.querySelector(".count-pending").innerHTML = getListCount().pending;
+    document.querySelector(".count-done").innerHTML = getListCount().done;
+}
+
+function init() {
+    updateListCount();
+}
+
+init();
+
+
+
+
+
+
