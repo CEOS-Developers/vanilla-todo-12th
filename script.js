8442718020051395d1225bbf815b7f8c26ec0676
bluewink <59544716+bluewink@users.noreply.github.com>
2020-09-17 17:03:53 +0900

    make local storage

--- script.js
+++ script.js
@@ -2,15 +2,41 @@ const toDoForm = document.querySelector(".toDo__form"),
   toDoInput = document.querySelector("input"),
   toDoWaitingItems = document.querySelector(".waiting__items"),
   toDoFinishedItems = document.querySelector(".finished__items"),
-  toDoInputIcon = document.querySelector(".input__icon");
+  toDoInputIcon = document.querySelector(".input__icon"),
+  toDoItem = document.querySelectorAll("li");
+
+const TODO_LS = "toDos";
+
+let toDos = [];
+
+function handleHover(event) {
+  event.target.style.color = "orange";
+  console.log("abc");
+}
+
+// function deleteToDo(event) {}
+
+function saveToDos() {
+  localStorage.setItem(TODO_LS, JSON.stringify(toDos));
+}
 
 function printToDo(text) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const delBtn = document.createElement("button");
+
+  //   delBtn.innerText = "🗑";
+  //   delBtn.addEventListener("click", deleteToDo);
 
   span.innerText = text;
   li.appendChild(span);
   toDoWaitingItems.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -21,8 +47,20 @@ function handleSubmit(event) {
 }
 
 function init() {
+  loadToDos();
   toDoForm.addEventListener("submit", handleSubmit);
   toDoInputIcon.addEventListener("click", handleSubmit);
+  toDoItem.forEach((li) => {
+    li.addEventListener("mouseenter", handleHover);
+  });
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODO_LS);
+  if (loadToDos !== null) {
+    const parseToDos = JSON.parse(loadedToDos);
+    parseToDos.forEach((toDo) => printToDo(toDo.text));
+  }
 }
 
 init();
