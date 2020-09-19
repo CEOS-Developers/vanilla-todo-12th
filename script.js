7e621ee036296fa6634b9c21162b93485ca1ce54
MoonSangJin <1128msj@naver.com>
2020-09-19 16:17:19 +0900

    Reflecting Code_Review from CEOS

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const toDoForm = document.querySelector(".thirdRow")
-    toDoInput = document.querySelector(".todoInput")
-    toDoList = document.querySelector(".firstList")
-    completeList = document.querySelector(".secondList")
-    count = document.querySelector(".firstCount")
-    count2 = document.querySelector(".secondCount")
-
+const toDoInput = document.querySelector(".todoInput")
+const toDoList = document.querySelector(".firstList")
+const completeList = document.querySelector(".secondList")
+const count = document.querySelector(".firstCount")
+const count2 = document.querySelector(".secondCount")
 
 let toDos = [];
 const TODOS_LS="toDos";
@@ -13,8 +12,6 @@ let toComplete = [];
 const COMPLETE_LS="toComplete";
 
 function deleteToDo(event){
-    console.log("delete check");
-    console.dir(event.target)
     const btn = event.target;
     const li = (btn.parentNode).parentNode; // btn.parentNode가 innerHtml로 인해 button 이었다. list를 삭제해야 하므로 한번더 parentNode 설정
     toDoList.removeChild(li);
@@ -27,7 +24,7 @@ function deleteToDo(event){
 
 function deleteOnlyToDo(evnet){
     const btn = event.target;
-    const li = btn.parentNode; // btn.parentNode가 innerHtml로 인해 button 이었다. list를 삭제해야 하므로 한번더 parentNode 설정
+    const li = btn.parentNode; // btn.parentNode가 여기서는 list 바로 나옴
     toDoList.removeChild(li);
     const cleanToDos = toDos.filter(function(toDo){
         return toDo.id !== parseInt(li.id);
@@ -37,7 +34,6 @@ function deleteOnlyToDo(evnet){
 }
 
 function deleteToComplete(event){
-    console.log("delete check");
     const btn = event.target;
     const li = (btn.parentNode).parentNode; // btn.parentNode가 innerHtml로 인해 button 이었다. list를 삭제해야 하므로 한번더 parentNode 설정
     completeList.removeChild(li);
@@ -51,77 +47,57 @@ function deleteToComplete(event){
 function saveToDos(){
     try{
         localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
-    
-        let countNumber = toDos.length;
-        console.log(countNumber); //현재 localStorage 속 개수 확인
-        count.innerHTML=countNumber; //현재 개수를 알려주자
-
+        const countNumber = toDos.length;
+        count.innerHTML=countNumber; //현재 todo 개수를 알려주자
     }catch(err){
         console.log("something wrong!");
     }
-    
 }
 
 function saveComplete(){
     try{
         localStorage.setItem(COMPLETE_LS,JSON.stringify(toComplete));
-    
-        let countNumber2 = toComplete.length;
-        console.log(countNumber2); //현재 localStorage 속 개수 확인
-        count2.innerHTML=countNumber2; //현재 개수를 알려주자
-
+        const countNumber2 = toComplete.length;
+        count2.innerHTML=countNumber2; //현재 complete 개수를 알려주자
     }catch(err){
         console.log("something wrong!");
     }
     
 }
 
-
 function moveComplete(text){
     //대기중에서 눌렸을때 실행 될 함수 1.대기중에서 delete, 2.완료에서 create,
-    console.log(text);
     paintToComplete(text);
 }
 
-function moveTo(event){
-    console.log("moveto check");
-}
-
 function paintToComplete(text){
-    console.log(text);
     const li = document.createElement("ul");
-
-    const delBtn = document.createElement("button");
-    delBtn.innerHTML="<img class=trash src=img/bin.png>";
-    delBtn.addEventListener("click",deleteToComplete);
-    
+    const delButton = document.createElement("button");
+    delButton.innerHTML="<img class=trash src=img/bin.png>";
+    delButton.addEventListener("click",deleteToComplete);
     const span = document.createElement("span");
     span.setAttribute("class","completeList");
     const newId = toComplete.length+1;
     span.innerText=text;   
-
     li.appendChild(span);
-    li.appendChild(delBtn);
+    li.appendChild(delButton);
     li.id = newId;
-
     completeList.appendChild(li);
 
     const toCompleteObj={
         text : text,
         id : newId
     };
+
     toComplete.push(toCompleteObj);
-    
     saveComplete(); //push 한 이후에 저장해야된당   
 }
 
 function paintToDo(text){
-    console.log(text);
     const li = document.createElement("ul");
-
-    const delBtn = document.createElement("button");
-    delBtn.innerHTML="<img class=trash src=img/bin.png>";
-    delBtn.addEventListener("click",deleteToDo);
+    const delButton = document.createElement("button");
+    delButton.innerHTML="<img class=trash src=img/bin.png>";
+    delButton.addEventListener("click",deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length+1;
     span.innerText=text;
@@ -132,19 +108,18 @@ function paintToDo(text){
     } 
 
     li.appendChild(span);
-    li.appendChild(delBtn);
+    li.appendChild(delButton);
     li.id = newId;
-
     toDoList.appendChild(li);
 
     const toDoObj={
         text : text,
         id : newId
     };
+
     toDos.push(toDoObj);
     saveToDos(); //push 한 이후에 저장해야된당   
 }
-
 
 function handleSubmit(event){
     event.preventDefault();
@@ -158,7 +133,6 @@ function handleSubmit(event){
 function loadToDos(){
     const loadedToDos=localStorage.getItem(TODOS_LS);
     if(loadedToDos!==null){
-        console.log("not empty");
         const parsedToDos = JSON.parse(loadedToDos);
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
@@ -171,7 +145,6 @@ function loadToDos(){
 function loadToComplete(){
     const loadedToComplete=localStorage.getItem(COMPLETE_LS);
     if(loadedToComplete!==null){
-        console.log("not empty");
         const parsedToDos = JSON.parse(loadedToComplete);
         parsedToDos.forEach(function(toComplete){
             paintToComplete(toComplete.text);
