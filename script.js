6d6d25208090d03708b49d9912e8362b5d9f0d59
Thea Seo <anatsolarborn@gmail.com>
2020-09-19 22:45:46 -0400

    feedback

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
-const taskInput = document.getElementById("todoInput");
-const addTaskBtn = document.getElementById("addButton"); //왜 안됌>?
-const pendingTasksList = document.getElementById("todosPendingList");
-const completedTasksList = document.getElementById("todosFinishedList");
+const taskInput = document.getElementById("todo-input");
+const addTaskBtn = document.getElementById("todo-submit-button"); //왜 안됌>?
+const pendingTasksList = document.getElementById("todos-pending-list");
+const completedTasksList = document.getElementById("todos-finished-list");
 
 
 //create New todo format
-let createNewTask = function (todo) {
+const createNewTask = function (todo) {
     let newTodo = document.createElement("li");
     let label = document.createElement("label");
     let deleteBtn = document.createElement("button");
 
     deleteBtn.className = "delete";
-    deleteBtn.innerHTML=`<img src="./img/bin.png", alt="bin", width=14px, height=14px>`;
+    deleteBtn.innerHTML = `<img src="./img/bin.png", alt="bin", width=14px, height=14px>`;
     label.innerText = todo;
-    label.className="label";
+    label.className = "label";
 
     newTodo.appendChild(label);
     newTodo.appendChild(deleteBtn);
@@ -23,60 +23,60 @@ let createNewTask = function (todo) {
 }
 
 //add Task
-let addTask = function () {
+const addTask = function () {
     console.log(" in add task");
     let newTask = createNewTask(taskInput.value); //.value??
     console.log(newTask)
     console.log(pendingTasksList)
     pendingTasksList.appendChild(newTask);
-    total=pendingTasksList.childElementCount
-    document.getElementById('pendingTasksNumber').innerHTML=total;
+    total = pendingTasksList.childElementCount
+    document.getElementById('pending-tasks-number').innerHTML = total;
     bindTaskEvents(newTask, completedTask);
 
     taskInput.value = "";
 }
 //tasks number
-let tasksCount=function(){
+const countTasks = function () {
     console.log('count tasks');
-    document.getElementById('finishedTasksNumber').innerHTML=completedTasksList.childElementCount;
-    document.getElementById('pendingTasksNumber').innerHTML=pendingTasksList.childElementCount;
+    document.getElementById('finished-tasks-number').innerHTML = completedTasksList.childElementCount;
+    document.getElementById('pending-tasks-number').innerHTML = pendingTasksList.childElementCount;
 
 }
 
-//delte Tasks
-let deleteTask = function () {
+//delete Tasks
+const deleteTask = function () {
     console.log("delete task");
 
     let tasksToDelete = this.parentNode;
-   /*
+
     let ul = tasksToDelete.parentNode;
     ul.removeChild(tasksToDelete);
-*/
 
-    tasksToDelete.parentNode
-    tasksCount();
+    countTasks();
 }
 
-let completedTask = function () {
+const completedTask = function () {
     console.log("completed Task");
     let listItem = this.parentNode;
-    listItem.style.textDecoration="line-through"
+    listItem.style.textDecoration = "line-through"
     completedTasksList.appendChild(listItem);
     bindTaskEvents(listItem, pendingTask);
 
 }
 
-let pendingTask = function () {
+const pendingTask = function () {
     console.log("incomplete Task");
     let listItem = this.parentNode;
-    if(listItem.style.textDecoration=="line-through"){
-        listItem.style.textDecoration="none";
+    if (listItem.style.textDecoration == "line-through") {
+        listItem.style.textDecoration = "none";
     }
     pendingTasksList.appendChild(listItem);
     //console.log("cnt",pendingTasksList.childElementCount)
-    
+
     bindTaskEvents(listItem, completedTask);
 }
+
+
 
 //write todo, add it!
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
@@ -90,19 +90,15 @@ function handleSubmitForm(e) {
 }
 
 //formulate a task
-let bindTaskEvents = function (taskListItem, statusHandler) {
+let bindTaskEvents = function (taskListItem, changeTasksStatus) {
     console.log("in bindTaskEvents");
     let changeStatus = taskListItem.getElementsByClassName('label')[0];
-    console.log(taskListItem);
     let deleteButton = taskListItem.getElementsByClassName('delete')[0];//ClassNamequerySelector("li.delete");
 
-    console.log(deleteButton);
-
     deleteButton.onclick = deleteTask;
-    
-    console.log('afterdelete');
+
     //클릭 시 넘어감
-    changeStatus.onclick = statusHandler;
- 
-    tasksCount();
+    changeStatus.onclick = changeTasksStatus;
+
+    countTasks();
 }
