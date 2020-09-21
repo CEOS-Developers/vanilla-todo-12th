919d7221d37e39548b30d2313b0828f865cb908a
Hooniya-nolja <wkdckdgns01@naver.com>
2020-09-21 10:33:34 +0900

    코드 리뷰들 반영 !!

--- script.js
+++ script.js
@@ -11,18 +11,19 @@ function addList (e) {
 
             // 껍데기 div부터 차례로 생성.
             let listbox = document.createElement('div');
-            listbox.className = 'listbox';
+            // listbox.className = 'listbox';
+            listbox.classList.add("listbox");
 
             // li 생성.
             let list = document.createElement('li');
-            list.className = "waiting";
-            list.innerHTML = todo;
+            list.classList.add("waiting");
+            list.innerHTML = todo;  //      
             list.setAttribute("onclick", "move(this)");
+
 
             // img 생성.
             let trash = document.createElement('img');
             trash.src = "./img/bin.png";
-            trash.alt = "";
             trash.setAttribute("onclick", "deleteList(this)");
 
             // div 안에 li와 img 이동.
@@ -30,13 +31,10 @@ function addList (e) {
             listbox.appendChild(trash);
 
             // li와 img가 들어간 div를 할 일 목록에 추가.
-            document.getElementById('waitList').append(listbox);
+            document.getElementById('wait-list').append(listbox);
 
             document.getElementById("add_input").value = '';
             checkListNum();
-
-            // localStorage.setItem("waiting", listbox);
-            // console.log('localStorage : ', localStorage);
         }
     }
 }
@@ -45,10 +43,10 @@ function addList (e) {
 function move(li) {
     if (li.className == "waiting") {    // 클릭한 것이 대기중인 일이라면 완료됨으로 이동.
         li.className = "compted";      // 클릭한 일이 대기중인지 완료됨인지 판단하는 기준.
-        document.getElementById("completeList").appendChild(li.parentNode);
+        document.getElementById("complete-list").appendChild(li.parentNode);
     } else {    // 완료된 일이라면 대기중으로 이동.
         li.className = "waiting";
-        document.getElementById("waitList").appendChild(li.parentNode);
+        document.getElementById("wait-list").appendChild(li.parentNode);
     }
 
     checkListNum();
@@ -62,13 +60,9 @@ function deleteList(li) {
 
 // 리스트의 할 일들 카운트
 function checkListNum() {
-    document.getElementsByClassName("wait")[0].innerHTML = `대기중 (${waitingNum.length})`;
-    document.getElementsByClassName("complete")[0].innerHTML = `완료됨 (${completeNum.length})`;
+    document.querySelector(".wait").textContent = `대기중 (${waitingNum.length})`;
+    document.querySelector(".complete").textContent = `완료됨 (${completeNum.length})`;
     // 다른 함수가 작동 할 때마다 이 함수를 동작하여 업데이트 해주어야 한다.
-}
-
-function storeLocal() {
-
 }
 
 checkListNum();
